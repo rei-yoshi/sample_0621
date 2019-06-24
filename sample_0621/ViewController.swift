4c8993a283ffb6f746d3d8c145fd135461743ff3
rei-yoshi <rei.yosida.5a@stu.hosei.ac.jp>
2019-06-24 21:21:36 +0900

    LINE Notifyの導入

--- sample_0621/ViewController.swift
+++ sample_0621/ViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
-
+import Alamofire
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var labeltest : UILabel!
+    @IBOutlet var buttonTest : UIButton!
+    
+    @IBOutlet weak var koment: UITextView!
+    var count = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labeltest.text = "Swift Test"
+        
+        buttonTest.setTitle("Button", for:UIControl.State.normal)
     }
-
-
+    func SendPostRequestToLineNotify(_ message: String) {
+        
+        let env = ProcessInfo.processInfo.environment
+        
+        guard let LINENOTIFY_TOKEN = env["LINENOTIFY_TOKEN"] else {
+            fatalError("LINENOTIFY_TOKENの値がnilです")
+        }
+        let url = "https://notify-api.line.me/api/notify"
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Bearer "+LINENOTIFY_TOKEN
+        ]
+        let parameters:[String: Any] = [
+            "message": message
+        ]
+        
+        AF.request(url,method: .post, parameters: parameters,encoding: URLEncoding.default,headers: headers).responseString {
+            response in
+            switch response.result {
+            case .success(let value):
+                self.labeltest.text = value
+            case .failure(let error):
+                self.labeltest.text = error.localizedDescription
+            }
+        }
+        
+    }
+    @IBAction func buttonTapped(_ sender :Any)
+    {
+        SendPostRequestToLineNotify(koment.text)
+        
+        
+    }
+    
+    
+    
 }
 
